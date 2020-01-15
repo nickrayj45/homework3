abf33bdf23aadcdff50a8a834e86defffc594222
Nicholas Jaquez <nrjaquez@gmail.com>
2020-01-14 17:58:31 -0800

    inital commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var passwords;
+
+passwords =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~abcdefghijklmnopqrstuvwxyz";
+
+function makePassword(passwords) {
+  return document.getElementById(
+    Math.floor(Math.random() * Math.floor(Math.random))
+  );
+}
