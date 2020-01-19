8c9192b6f9b8850a4fa1fcce9f8b0e549d5b8e1a
Nicholas Jaquez <nrjaquez@gmail.com>
2020-01-18 18:26:26 -0800

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is the password generator.
 
 You can click on the "Create Button" to generate a random 8-128 character password with lowercase letters, uppercase letters, symobols, and numbers.
+
+An alert will generate the random password.
--- script.js
+++ script.js
@@ -89,61 +89,56 @@ function makeNumber() {
   getNumber = number[numbers];
   console.log(getNumber);
   console.log(number[getNumber]);
-
-  // Function for randomly pulling a lowercase letter
-  function makeSmallLetter() {
-    var small = Math.floor(Math.random() * (25 - 0) + 0);
-    smallLetter = lower[small];
-    console.log(smallLetter);
-    console.log(lower[smallLetter]);
-  }
-
-  // Function for randomly pulling symbol
-  function makeSymbol() {
-    var chars = Math.floor(Math.random() * (10 - 0) + 0);
-    getSymbol = symbols[chars];
-    console.log(getSymbol);
-    console.log(symbols[getSymbol]);
-  }
-
-  // Function for randomly pulling a mix of the variable between 8 and 128
-  function chooseElement() {
-    getElement = Math.floor(Math.random() * (32 - 2) + 0);
-    console.log(getElement);
-    console.log(getElement);
-    // return getElement;
-  }
-
-  chooseElement();
-
-  // Create password for loop with the functions created above
-  for (var i = 0; i < getElement; i++) {
-    makeLetter();
-    makeNumber();
-    makeSmallLetter();
-    makeSymbol();
-    pswdstring += letter;
-    pswdstring += getNumber;
-    pswdstring += smallLetter;
-    pswdstring += getSymbol;
-  }
-
-  //Create the array
-
-  // Create the prototype to push
-
-  // Create an array that will push the values from lines 17 and 26 to the end of an array
-  // once we have the 4 requirements,i need to join the arrays together. That joined array will be the password.
-  // Turn array into a string
-
-  console.log("THE LETTER IS:" + pswdstring);
 }
 
-// Create copy to clipboard function
+// Function for randomly pulling a lowercase letter
+function makeSmallLetter() {
+  var small = Math.floor(Math.random() * (25 - 0) + 0);
+  smallLetter = lower[small];
+  console.log(smallLetter);
+  console.log(lower[smallLetter]);
+}
 
-// function myFunction() {
-//   var copyText = document.getElementById("#copyPswd");
-//   copyText.setSelectionRange(0, 99999);
-//   document.execCommand("copy");
-//   alert("Copied the text: " + copyText.value);
-// }
+// Function for randomly pulling symbol
+function makeSymbol() {
+  var chars = Math.floor(Math.random() * (10 - 0) + 0);
+  getSymbol = symbols[chars];
+  console.log(getSymbol);
+  console.log(symbols[getSymbol]);
+}
+
+// Function for randomly pulling a mix of the variable between 8 and 128
+function chooseElement() {
+  getElement = Math.floor(Math.random() * (32 - 2) + 0);
+  console.log(getElement);
+  console.log(getElement);
+  // return getElement;
+}
+
+chooseElement();
+
+// Create password for loop with the functions created above
+for (var i = 0; i < getElement; i++) {
+  makeLetter();
+  makeNumber();
+  makeSmallLetter();
+  makeSymbol();
+  pswdstring += letter;
+  pswdstring += getNumber;
+  pswdstring += smallLetter;
+  pswdstring += getSymbol;
+}
+
+document.getElementById("createBtn").addEventListener("click", function() {
+  alert(pswdstring);
+});
+//Create the array
+
+console.log("THE LETTER IS:" + pswdstring);
+
+// Create copy to clipboard function
+function copyToClickboard() {
+  var copyText = document.getElementById("pswdstring");
+  document.execCommand("copyToClipboard");
+  alert("Copied the text: " + copyText.value);
+}
